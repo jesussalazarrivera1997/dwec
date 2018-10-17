2d89357faaba077729e1f22e01c289578f688b81
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2018-10-17 12:24:46 +0200

    si

---
+++ ejercicios/tema 4/worshit 1/ejercicio9.js
@@ -0,0 +1,11 @@
+/*9.
+Crea una página web que haciendo uso de eventos permita arrastrar una imagen
+haciendo uso del ratón (al pulsar sobre la imagen la voy arrastrando hasta que suelto
+el botón)
+*/
+function arrastrarElemento(){
+    let elemnto = document.getElementById("elementoArrastrable");
+    
+}
+
+document.getElementById("elementoArrastrable").addEventListener("",arrastrarElemento);
