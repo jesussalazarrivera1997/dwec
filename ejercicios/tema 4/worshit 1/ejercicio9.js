5fa62dd49921d502784eb7c154559ce49ff02e3e
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2018-10-22 14:30:40 +0200

    s

--- ejercicios/tema 4/worshit 1/ejercicio9.js
+++ ejercicios/tema 4/worshit 1/ejercicio9.js
@@ -3,6 +3,7 @@ Crea una página web que haciendo uso de eventos permita arrastrar una imagen
 haciendo uso del ratón (al pulsar sobre la imagen la voy arrastrando hasta que suelto
 el botón)
 */
+/*
 function creaTables(ancho, alto, numfilas, numcolum) {
     var totalAcho = ancho * numcolum;
     contador=0;
@@ -36,3 +37,39 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 creaTables(50, 50, 8, 8);
+*/
+ball.onmousedown = function (event) {
+
+    let shiftX = event.clientX - ball.getBoundingClientRect().left;
+    let shiftY = event.clientY - ball.getBoundingClientRect().top;
+
+    ball.style.position = 'absolute';
+    ball.style.zIndex = 1000;
+    document.body.append(ball);
+
+    moveAt(event.pageX, event.pageY);
+
+    // centers the ball at (pageX, pageY) coordinates
+    function moveAt(pageX, pageY) {
+        ball.style.left = pageX - shiftX + 'px';
+        ball.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    // (3) move the ball on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // (4) drop the ball, remove unneeded handlers
+    ball.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        ball.onmouseup = null;
+    };
+
+};
+
+ball.ondragstart = function () {
+    return false;
+};
