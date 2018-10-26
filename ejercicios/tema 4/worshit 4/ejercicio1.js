5913b19d9bbf993e51e14066c8813403d03a43ba
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2018-10-26 11:05:42 +0200

    s

--- ejercicios/tema 4/worshit 4/ejercicio1.js
+++ ejercicios/tema 4/worshit 4/ejercicio1.js
@@ -1,4 +1,5 @@
 /*
+
 1)
 Diseña una web que hará uso de cookies para guardar el nombre del usuario. 
 En caso de no tener ninguna cookie de ese sitio, deberá preguntar el nombre
@@ -6,11 +7,16 @@ del usuario y almacenarlo en una cookie que caducará en 5 minutos.
 Tras esto saludará al usuario mediante un mensaje en pantalla. 
 En caso de tener ya creada la cookie deberá leerla y mostrar el mensaje anterior directamente. 
 Deberá proporcionar también un enlace para borrar la cookie (lo que podríamos llamar 'Cerrar Sesión').
+
+2) Mejora el ejercicio anterior dando al usuario las opciones para que pueda configurar el color de fondo,
+de párrafo y el tamaño de la letra.
+
+3) ¿Cómo podemos hacer que la segunda cookie se borre automáticamente al cerrar el navegador? Compruébalo
 */
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + (exdays  * 60 * 1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
@@ -37,7 +43,31 @@ function checkCookie() {
     } else {
         user = prompt("Please enter your name:", "");
         if (user != "" && user != null) {
-            setCookie("username", user, 365);
+            setCookie("username", user, 5);
         }
     }
+};
+checkCookie();
+
+function delete_cookie( name ) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  }
+function cerrarSesion(){
+    delete_cookie("username");
 }
+
+function cambiarattributos(){
+    function cambiarLetra(){
+        let letras = document.getElementById("letras");
+        document.body.style.fontSize = document.body.style.fontSize+letras[0];
+    }
+    function cambiarcolor(){
+        let color = document.getElementById("color");
+        document.body.style.color = color[0];
+    }
+    document.getElementById("").addEventListener("click", cambiarLetra);
+    document.getElementById("").addEventListener("click", cambiarcolor);
+    
+
+}
+document.getElementById("cerrarSesion").addEventListener("click",cerrarSesion)
